4c0c22544280d1533be7e945fb276c4124dac886
marianacarbonell <marianacarbonellm@gmail.com>
2019-02-27 15:50:57 -0300

    da los links pero no dice cuales están rotos

--- index.js
+++ index.js
@@ -16,18 +16,21 @@ const figlet = require ("figlet"); // tipografía
 const minimist = require ("minimist"); // ayuda a leer los parámetros (que da el usuario)
 const fs = require ('fs' ); // Permite manejar los archivos
 const read = process.argv[2]
+const markdownLinkExtractor = require('markdown-link-extractor');
 
 // leer el archivo 
 function readFolder (read){
-  fs.readFile(read,'utf8', (err,data)=>{
-    if(data){
-      console.log(data)
-    }else{
-      console.log(err)
-    }
-})
+  let markdown = fs.readFileSync(read).toString()
+  const links = markdownLinkExtractor(markdown);
+  links.forEach(function (link) {
+    console.log(link);
+});
 };
 readFolder(read);
+
+
+
+
 
 const validateFolder = (path) =>{ // dice si sirve o no sirve(si es valido o no)
   if (fs.existsSync(path)){ //valida la ruta  
