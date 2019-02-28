c90c1e788a2ccf839623d4c6aa6b30d248aa7500
marianacarbonell <marianacarbonellm@gmail.com>
2019-02-28 12:05:23 -0300

    pasa los links con los status

--- index.js
+++ index.js
@@ -10,48 +10,61 @@
 // }
 // console.log(data.toString()); 
 // });
+//const figlet = require ("figlet"); // tipografía
+//const minimist = require ("minimist"); // ayuda a leer los parámetros (que da el usuario)
 
-const chalk = require ("chalk");
-const figlet = require ("figlet"); // tipografía
-const minimist = require ("minimist"); // ayuda a leer los parámetros (que da el usuario)
-const fs = require ('fs' ); // Permite manejar los archivos
+
+const chalk = require("chalk");
+const fs = require('fs'); // Permite manejar los archivos
 const read = process.argv[2]
 const markdownLinkExtractor = require('markdown-link-extractor');
-//const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 
-// leer el archivo 
-function readFolder (read){
-  return new Promise((res,reject) => {
-  let markdown = fs.readFileSync(read).toString()
-  const links = markdownLinkExtractor(markdown);
-  console.log(links)
-//   links.forEach(function (link) {
-//     console.log(link);
-// });
-const fetchLinks = fetch(links).then(res => {
-  const objectLinks = {
-    url :res.url,
-    statusLinks: res.status,
-    statusText: res.statusText
-  };
-  return objectLinks;
-  console.log(res)
-})
-.catch(error =>{
-  console.log(error)
-  }
-)
-})
+function readFolder(read) {
+  let promise = new Promise((res, reject) => {
+    let markdown = fs.readFileSync(read).toString()
+    const links = markdownLinkExtractor(markdown);
+    console.log(links)
+    //   links.forEach(function (link) {
+    //     console.log(link);
+    // });
+    const arrPromise = [];
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+      const fetchLinks = fetch(link).then(res => {
+        const objectLinks = {
+          url: res.url,
+          statusLinks: res.status,
+          statusText: res.statusText
+        };
+        return objectLinks;
+      })
+        .catch(error => {
+          const objectLinks = {
+            url: link,
+            statusLinks: "FAIL",
+          };
+          return objectLinks;
+        });
+        arrPromise.push(fetchLinks);
+    }
+    Promise.all(arrPromise).then(res =>{
+    console.log(res)
+    })
+  })
 };
-
 readFolder(read);
+
+
+
+
 
 
 // Validar archivo
 // const validateFolder = (path) =>{ 
 //   if (fs.existsSync(path)){ //valida la ruta  
 //   if(fs.statSync(path).isFile()){ // verifica que sea un archivo
-  
+
 //     const arrString = path.split(".");
 //     if(arrString[1] === "md"){
 //       return true;
